39e37553b039fa057e68d4e790c81806edf52eee
Showe93 <showe1364@gmail.com>
2020-12-16 12:48:43 -0500

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,23 +16,25 @@ public class Main {
 	public static void main(String[] args) {
 		int[] data = {71, 39, 38, 68};
 		int n = data.length;
+		int storeSpot;
+		int swapSpot;
 		// Declare other variables as needed.
 		for (int i = 0; i < n-1; i++) {
 			// At each pass through outer loop, storeSpot moves right to left. (Position 0 is automatic.)
 			 // Confirm storeSpot for debugging purposes.
 			// Initialize any other variables as needed.
 
-			int idx = i; //Declaring the int idx
-			for (int k = i + 1; k < n; k++) {
-				if (data[k] < data[idx]){ //Check if the value at a certain place in the array is less then the value currently stored inside data[idx]
-						idx= k;
+			storeSpot = i; //Declaring the int idx
+			for (swapSpot = i + 1; swapSpot < n; swapSpot++) {
+				if (data[swapSpot] < data[storeSpot]){ //Check if the value at a certain place in the array is less then the value currently stored inside data[idx]
+						storeSpot = swapSpot;
 				}
 
 			}
 
 
-			int temp = data[idx]; //declare temp so that the new can be sorted appropriately into the list
-			data[idx]=data[i];
+			int temp = data[storeSpot]; //declare temp so that the new can be sorted appropriately into the list
+			data[storeSpot]=data[i];
 			data[i]=temp;
 
 		}
